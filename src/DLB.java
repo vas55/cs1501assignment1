11fcda6321cff2e29abbb84f91ef25113813d92a
vas55 <vas55@pitt.edu>
2017-09-25 01:19:38 -0400

    searching works, working on implementing userDLB

--- src/DLB.java
+++ src/DLB.java
@@ -8,6 +8,8 @@ public class DLB {
 		String nextLetter = "";
 		LinkedList dictLL = new LinkedList();
 		LinkedList userDLB = new LinkedList();
+		char[] w = new char[100];
+		int index = 0;
 		
 		// create dictionary DLB and user DLB
 		System.out.println("Started scanning dictionary file");
@@ -26,8 +28,7 @@ public class DLB {
 		
 		dictionary.close();
 		System.out.println("Done inputting dictionary!");
-		
-		
+				
 		// create DLB for user_history
 		Scanner userHistory = new Scanner(new File("user_history.txt"));
 		while (userHistory.hasNext()) {
@@ -39,50 +40,59 @@ public class DLB {
 
 		System.out.println("Enter your character: ");
 		Scanner sc = new Scanner(System.in);
-		String userInput = sc.next().toLowerCase();
+		String userInput = sc.nextLine().toLowerCase();
 		// user will input by one character, so we will enter the user input into an array and convert to a full string when searching
 		// DO THIS
-		char[] w = new char[100];
-		int index = 0;
-		Node n = dictLL.reachLastNodeInPrefix(userInput);
-		//using one down node from prefix
-		//need to keep track of WordLIst index 
-		int openIndex = 0;
-		if (n == null) {
-			System.out.println("No suggestions");
-			//add to user history directly
-			//DO THIS STILL
-		}else {
-			dictLL.search(userInput, n.down, w, index);
+		
+		//System.out.println("userInput=" + userInput);
+		if (userInput.equals("$")) {
+			System.out.println("End of word");
 		}
-
-		int count = 0;
-		String[] wordList = dictLL.returnWordList();
-		for (int i=0; i<wordList.length; i++) {
-			if (count < 5){
-				try {
-					if (wordList[i] == null) {
-						break;
+		else if (userInput.equals("!")){
+			System.out.println("End of program. Bye");
+		}
+		else {
+			Node n = dictLL.reachLastNodeInPrefix(userInput);
+			//using one down node from prefix
+			//need to keep track of WordLIst index 
+			if (n == null) {
+				//System.out.println("No suggestions");
+				//add to user history directly
+				//DO THIS STILL
+			}else {
+				dictLL.search(userInput, n.down, w, index);
+			}
+	
+			int count = 0;
+			String[] wordList = dictLL.returnWordList();
+			for (int i=0; i<wordList.length; i++) {
+				
+				if (count < 5){
+					try {
+						if (wordList[i] == null) {
+							break;
+						}
+						else {
+							//check if contained in user history
+							String outputSuggestion = wordList[i].replaceAll("\0", "");
+							System.out.println(userInput + outputSuggestion);
+							count++;
+						}
+					}catch (NullPointerException e) {
+						System.out.println("");
 					}
-					else {
-						String outputSuggestion = wordList[i].replaceAll("\0", "");
-						System.out.println(userInput + outputSuggestion);
-						count++;
-					}
-				}catch (NullPointerException e) {
-					System.out.println("");
 				}
-			}
-			else {
-				break;
-			}
-		}
+				else {
+					break;
+				}
+			} // end for loop
+		} // end else 
 		System.out.println("Done running...");
 		//create DLB for user
 		//userDLB();
 		//dictionaryDLB();
 		//userInput();
-	}
+	} // end main
 	
 	public static void userInput () throws IOException {
 		Scanner sc = new Scanner(System.in);
